df5857ac8502f8ea09f8eee22b1310ee306ee99b
prithivilaksh <prithivilaksh@gmail.com>
2025-08-12 22:47:37 +0530

    Delete elevatorGroup directory

--- elevatorGroup/elevatorgroup.go
+++
@@ -1,8 +0,0 @@
-package elevatorgroup
-
-import "github.com/prithivilaksh/elevator-system/elevator"
-
-type ElevatorGroup interface {
-	AddElevator(elevator *elevator.Elevator)
-	GetElevatorID(from int, to int) (int, error)
-}
